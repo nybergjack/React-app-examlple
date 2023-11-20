3099ca2e87b16bd629d69465974eb74182c44e38
Jack nyberg <nybergjack@gmail.com>
2023-11-20 18:41:11 +0100

    added eslint

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersApi } from './api/usersApi';
 import authSlice from './slices/authSlice';
+
+const middlewares = [usersApi.middleware]
+
+if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+    middlewares.push(logger);
+    }
+
 export const store = configureStore({
 reducer: {
 [usersApi.reducerPath]: usersApi.reducer,
 auth: authSlice
 },
 middleware: (getDefaultMiddleware) =>
-getDefaultMiddleware().concat(usersApi.middleware),
+getDefaultMiddleware().concat(...middlewares),
 });
 setupListeners(store.dispatch);
